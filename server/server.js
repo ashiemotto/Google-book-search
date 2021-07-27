41bc0bae855598a2ffce035fbac6c56da82d9022
Ashiemotto <ashheadley@gmail.com>
2021-07-27 14:02:37 -0400

    update

--- server/server.js
+++ server/server.js
@@ -7,16 +7,17 @@ const {authMiddleware} = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const startServer = async () => {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-
+await server.start();
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -35,3 +36,5 @@ db.once('open', () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
+};
+startServer();
